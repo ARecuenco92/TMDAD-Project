ed279ba4456fa0633b431976c7908a2e89e6a544
ARecuenco92 <alvaro.recuenco92@gamil.com>
2015-04-15 00:37:04 +0200

    Timeline

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-	timelineSearch();
+	setupTimeline();
 });
 
-function timelineSearch() {
+function setupTimeline() {
 	$.get('timeline', function(data) {
-		var template = $('#timeline').html();
+		var template = $('#twitterBlock').html();
 		Mustache.parse(template); 
 		var rendered = Mustache.render(template, data);
-		$('#timelineBlock').append(rendered);
+		$('#timelineTwitter').append(rendered);
 	});	
+	
+	$('#twitter').click(function() {
+		$('#timelineTwitter').css("display", "block");
+		$('#timelineFacebook').css("display", "none");
+	});
+	
+	$('#facebook').click(function() {
+		$('#timelineTwitter').css("display", "none");
+		$('#timelineFacebook').css("display", "block");
+	});
+	
 }
