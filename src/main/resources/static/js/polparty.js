ec23e6ad1776fd0f53dab67500e8318fd78cc147
ARecuenco92 <alvaro.recuenco92@gamil.com>
2015-05-23 16:58:59 +0200

    Páginas partidos plíticos

--- src/main/resources/static/js/polparty.js
+++ src/main/resources/static/js/polparty.js
@@ -2,6 +2,19 @@ var politicalParty;
 
 $(document).ready(function() {
 	if(window.location.hash) {
-		var politicalParty = window.location.hash.substring(1);
+		politicalParty = window.location.hash.substring(1);
+		$("[id='"+politicalParty+"']").parent().addClass('active');
 	}
+	$(".party").click(displayPoliticalParty);
 });
+
+function displayPoliticalParty(event){
+	event.preventDefault();
+	var nextParty = $(this).attr('id');
+	if(nextParty != politicalParty){
+		$("[id='"+politicalParty+"']").parent().removeClass('active');
+		$("[id='"+nextParty+"']").parent().addClass('active');
+		document.location.hash = nextParty;
+		politicalParty = nextParty;
+	}
+}
