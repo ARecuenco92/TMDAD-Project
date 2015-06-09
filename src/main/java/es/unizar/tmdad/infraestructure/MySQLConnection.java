a29e6381b9ef7029ce5f7b9b73c3691c08cbeb69
ARecuenco92 <alvaro.recuenco92@gamil.com>
2015-06-09 08:52:27 +0200

    MySQLConnection to Openshift

--- src/main/java/es/unizar/tmdad/infraestructure/MySQLConnection.java
+++ src/main/java/es/unizar/tmdad/infraestructure/MySQLConnection.java
@@ -6,23 +6,27 @@ import java.sql.SQLException;
 
 public class MySQLConnection {
 	private final static String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
-	private final static String DRIVER_URL = "jdbc:mysql://localhost:3306/politic_app";
-	private final static String USER = "";
-	private final static String PASSWORD = "";
-	
+	static String APP_NAME = System.getenv("OPENSHIFT_APP_NAME");
+	static String DB_HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+	static String DB_PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+	static String DB_USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	static String DB_PASSWD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+
+	private final static String DRIVER_URL = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + APP_NAME;
+
 	static {
-		
+
 		try {
 			Class.forName(DRIVER_CLASS_NAME);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace(System.err);
 		}
-		
+
 	}
 
 	private MySQLConnection() {}
 
 	public final static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(DRIVER_URL, USER, PASSWORD);
+		return DriverManager.getConnection(DRIVER_URL, DB_USER, DB_PASSWD);
 	}
 }
